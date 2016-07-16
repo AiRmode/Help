fdd626e93358007ddb2ff8361bd402daf4eff76d
Maksym <chernikov2297@gmail.com>
2016-07-16 11:27:15 +0300

    Data model API added

--- data-model-api/src/main/java/com/formula1/help/forum/Topic.java
+++
@@ -1,7 +0,0 @@
-package com.formula1.help.forum;
-
-/**
- * Created by alexey on 05.05.16.
- */
-public interface Topic {
-}
---
+++ data-model-api/src/main/java/com/provectus/prodobro/actor/company/Company.java
@@ -0,0 +1,39 @@
+package com.provectus.prodobro.actor.company;
+
+
+import com.provectus.prodobro.actor.Actor;
+import com.provectus.prodobro.actor.user.User;
+import com.provectus.prodobro.event.Event;
+
+import java.util.List;
+
+public interface Company extends Actor {
+
+    String getTitle();
+
+    List<String> getAlias();
+
+    String getDescription();
+
+    /**
+     * @return {@link List<User>} of assigned users
+     */
+    List<User> getUsers();
+
+    /**
+     * @return {@link List<User>} of company assigned events
+     */
+    List<Event> getCompanyAssignedEvents();
+
+    void setTitle(String title);
+
+    void setAlias(List<String> alias);
+
+    void setDescription(String description);
+
+    void setUsers(List<User> users);
+
+    void setCompanyAssignedEvents(List<Event> companyAssignedEvents);
+
+
+}
