5ed64c04e744ba72b4b2596bfc2f6db82db54bf2
Valerij Stankov <gallardo_666@mail.ru>
2016-10-15 19:01:15 +0300

    User Tests refactoring

--- services-api-impl/src/test/java/com/provectus/prodobro/dao/UserDAOTest.java
+++ services-api-impl/src/test/java/com/provectus/prodobro/dao/UserDAOTest.java
@@ -23,102 +23,91 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDAOTest {
 
-    @Autowired
-    private UserDAO userDAO;
+	@Autowired
+	private UserDAO userDAO;
 
-    private User user;
+	private User user;
 
-    @Before
-    public void setUpData() {
-        user = new UserImpl();
-        user.setName("User");
-        user.setEmail("Email");
-        user.setPhoneNumber("Phone");
-        user.setPassHash("Pass");
+	@Before
+	public void setUpData() {
+		user = new UserImpl();
+		user.setName("User");
+		user.setEmail("Email");
+		user.setPhoneNumber("Phone");
+		user.setPassHash("Pass");
 
-        Status userStatus = new UserStatus();
-        userStatus.setStatus("ACTIVE");
-        user.setStatus(userStatus);
+		Status userStatus = new UserStatus();
+		userStatus.setStatus("ACTIVE");
+		user.setStatus(userStatus);
 
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        user.setCreatedDate(timestamp);
-        user.setLastModifiedDate(timestamp);
-    }
+		Timestamp timestamp = new Timestamp(new Date().getTime());
+		user.setCreatedDate(timestamp);
+		user.setLastModifiedDate(timestamp);
+	}
 
-    @Test
-    @Transactional
-    @Rollback
-    public void userDAOTest() {
-        userDAO.save(user);
-        getByIdTest();
-        getAllTest();
-        getByNameTest();
-        getByStatusTest();
-        getByPhoneNumberTest();
-        getByEmailTest();
-        getByLoginAndPasswordTest();
-    }
+	@Test
+	@Transactional
+	@Rollback
+	public void userDAOTest() {
+		userDAO.save(user);
+		getByIdTest();
+		getAllTest();
+		getByNameTest();
+		getByStatusTest();
+		getByPhoneNumberTest();
+		getByEmailTest();
+		getByLoginAndPasswordTest();
+	}
 
-    private void getByIdTest() {
-        User userFromDB = userDAO.getById(user.getId());
+	private void getByIdTest() {
+		User userFromDB = userDAO.getById(user.getId());
 
-        Assert.assertEquals(user.getName(), userFromDB.getName());
-        Assert.assertEquals(user.getEmail(), userFromDB.getEmail());
-        Assert.assertEquals(user.getPhoneNumber(), userFromDB.getPhoneNumber());
-    }
+		Assert.assertEquals(user.getName(), userFromDB.getName());
+		Assert.assertEquals(user.getEmail(), userFromDB.getEmail());
+		Assert.assertEquals(user.getPhoneNumber(), userFromDB.getPhoneNumber());
+	}
 
-    private void getAllTest() {
-        List<User> users = userDAO.getAll();
+	private void getAllTest() {
+		List<User> users = userDAO.getAll();
+		if (users.size() < 1) Assert.fail();
+	}
 
-        Assert.assertEquals(1, users.size());
-        Assert.assertEquals(user.getId(), users.get(0).getId());
-        Assert.assertEquals(user.getName(), users.get(0).getName());
-        Assert.assertEquals(user.getEmail(), users.get(0).getEmail());
-        Assert.assertEquals(user.getPhoneNumber(), users.get(0).getPhoneNumber());
-    }
+	private void getByNameTest() {
+		List<User> users = userDAO.getByName(user.getName());
 
-    private void getByNameTest() {
-        List<User> users = userDAO.getByName(user.getName());
+		Assert.assertEquals(user.getId(), users.get(0).getId());
+		Assert.assertEquals(user.getEmail(), users.get(0).getEmail());
+		Assert.assertEquals(user.getPhoneNumber(), users.get(0).getPhoneNumber());
+	}
 
-        Assert.assertEquals(1, users.size());
-        Assert.assertEquals(user.getId(), users.get(0).getId());
-        Assert.assertEquals(user.getEmail(), users.get(0).getEmail());
-        Assert.assertEquals(user.getPhoneNumber(), users.get(0).getPhoneNumber());
-    }
+	private void getByStatusTest() {
+		List<User> users = userDAO.getByStatus(user.getStatus().getStatus());
+		if (users.size() < 1) Assert.fail();
+	}
 
-    private void getByStatusTest() {
-        List<User> users = userDAO.getByStatus(user.getStatus().getStatus());
+	private void getByPhoneNumberTest() {
+		User userFromDB = userDAO.getByPhoneNumber(user.getPhoneNumber());
 
-        Assert.assertEquals(1, users.size());
-        Assert.assertEquals(user.getId(), users.get(0).getId());
-        Assert.assertEquals(user.getName(), users.get(0).getName());
-        Assert.assertEquals(user.getEmail(), users.get(0).getEmail());
-        Assert.assertEquals(user.getPhoneNumber(), users.get(0).getPhoneNumber());
-    }
+		Assert.assertEquals(user.getId(), userFromDB.getId());
+		Assert.assertEquals(user.getName(), userFromDB.getName());
+		Assert.assertEquals(user.getEmail(), userFromDB.getEmail());
+	}
 
-    private void getByPhoneNumberTest() {
-        User userFromDB = userDAO.getByPhoneNumber(user.getPhoneNumber());
+	private void getByEmailTest() {
+		User userFromDB = userDAO.getByEmail(user.getEmail());
 
-        Assert.assertEquals(user.getId(), userFromDB.getId());
-        Assert.assertEquals(user.getName(), userFromDB.getName());
-        Assert.assertEquals(user.getEmail(), userFromDB.getEmail());
-    }
+		Assert.assertEquals(user.getId(), userFromDB.getId());
+		Assert.assertEquals(user.getName(), userFromDB.getName());
+		Assert.assertEquals(user.getPhoneNumber(), userFromDB.getPhoneNumber());
+	}
 
-    private void getByEmailTest() {
-        User userFromDB = userDAO.getByEmail(user.getEmail());
+	private void getByLoginAndPasswordTest() {
+		User userByEmail = userDAO.getByLoginAndPassword(user.getEmail(), user.getPassHash());
+		User userByPhoneNum = userDAO.getByLoginAndPassword(user.getPhoneNumber(), user.getPassHash());
 
-        Assert.assertEquals(user.getId(), userFromDB.getId());
-        Assert.assertEquals(user.getName(), userFromDB.getName());
-        Assert.assertEquals(user.getPhoneNumber(), userFromDB.getPhoneNumber());
-    }
-
-    private void getByLoginAndPasswordTest() {
-        User userByEmail = userDAO.getByLoginAndPassword(user.getEmail(), user.getPassHash());
-        User userByPhoneNum = userDAO.getByLoginAndPassword(user.getPhoneNumber(), user.getPassHash());
-
-        Assert.assertEquals(userByEmail.getEmail(), userByPhoneNum.getEmail());
-        Assert.assertEquals(userByEmail.getName(), userByPhoneNum.getName());
-        Assert.assertEquals(userByEmail.getPhoneNumber(), userByPhoneNum.getPhoneNumber());
-        Assert.assertEquals(userByEmail.getId(), userByPhoneNum.getId());
-    }
+		Assert.assertEquals(userByEmail.getEmail(), 		userByPhoneNum.getEmail());
+		Assert.assertEquals(userByEmail.getName(),			userByPhoneNum.getName());
+		Assert.assertEquals(userByEmail.getPhoneNumber(), 	userByPhoneNum.getPhoneNumber());
+		Assert.assertEquals(userByEmail.getId(), 			userByPhoneNum.getId());
+	}
 }
