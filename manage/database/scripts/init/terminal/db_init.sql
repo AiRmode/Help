ef8b25c75973a76f0810926e8f6fa48bdd41015f
Roman Garayev <romangarayev@gmail.com>
2016-10-19 20:02:40 +0300

    Bug fixes

--- manage/database/scripts/init/terminal/db_init.sql
+++ manage/database/scripts/init/terminal/db_init.sql
@@ -198,7 +198,7 @@ CREATE TABLE :schema_name."email_queue" (
   attachment_id INTEGER,
   created_date  TIMESTAMP    NOT NULL,
   status        VARCHAR(20)  NOT NULL,
-  send_date     TIMESTAMP    NOT NULL,
+  send_date     TIMESTAMP,
 
   PRIMARY KEY (email_id)
 );
@@ -291,23 +291,5 @@ VALUES
   ('NOT_VERIFIED', 0),
   ('ACTIVE', 1)
 ON CONFLICT DO NOTHING;
-
-
-INSERT INTO :schema_name."user" (id, name, email, pass_hash, phone_num, status_id,
-                                is_superuser, created_date, last_modified_date)
-VALUES
-  (1, 'admin', 'example@mail.com', '120b6ae92e26693f1f0f77a9cc91e87a',
-   '+38 097 777 77 77', 1, TRUE, '2000-01-01 00:00:00', '2000-01-01 00:00:00')
-ON CONFLICT (id)
-  DO UPDATE SET
-    name	 = EXCLUDED.name,
-    pass_hash	 = EXCLUDED.pass_hash,
-    phone_num	 = EXCLUDED.phone_num,
-    email 	 = EXCLUDED.email;
-
---pass_hash = (MD5) adm1npass
---if exist -> update name, pass_hash, phone_num, email
-
-
 
 COMMIT;
